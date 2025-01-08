8131fd976d9cb5677ca95d06e6f0ef70df5ba427
unknown <adrianteisanu@gmail.com>
2025-01-08 23:27:54 +0530

    test

--- at-ec2/lib/at-ec2-stack.ts
+++ at-ec2/lib/at-ec2-stack.ts
@@ -1,3 +1,4 @@
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -6,11 +7,16 @@ export class AtEc2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AtEc2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+   // define default vpc 
+   const vpc= ec2.Vpc.fromLookup(this,'ExistingVPC',{
+      isDefault:true
+   });
+   // creating EC2 Instance 
+   const at_inst= new ec2.Instance(this,'at_machine',{
+      vpc,
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: new ec2.AmazonLinuxImage(),
+      keyName: 'splunk-key',
+   });
   }
 }
